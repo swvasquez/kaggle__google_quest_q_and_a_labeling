48bf512b4c5d381f2c88aa3df3c0711e1903d4b9
Scott W. Vasquez <SWVasquez@outlook.com>
2020-01-29 19:02:22 -0800

    added sentence lengths and newlines as features. changed dataset generator to yield a dictionary of arrays. functional.

--- src/model/train_model.py
+++ src/model/train_model.py
@@ -1,67 +1,23 @@
-import psutil
+import datetime
 import subprocess
 import time
 
 import numpy as np
+import psutil
 import scipy
-import transformers
 import torch
+import transformers
 import yaml
 
 from src import project_paths
 from src.data.dataset import KFoldDataset
 from src.data.redis_db import redis_cnxn
-
-
-class DistilBertForQUEST(transformers.DistilBertPreTrainedModel):
-
-    def __init__(self, config):
-        super().__init__(config)
-        self.num_labels = config.num_labels
-
-        self.distilbert = transformers.DistilBertModel(config)
-        self.pre_classifier = torch.nn.Linear(3077, 3077)
-        self.classifier = torch.nn.Linear(3077, config.num_labels)
-        self.init_weights()
-
-    def forward(self,
-                input_ids=None,
-                attention_mask=None,
-                head_mask=None,
-                inputs_embeds=None,
-                labels=None,
-                category=None):
-
-        distilbert_output = self.distilbert(
-            input_ids=input_ids,
-            attention_mask=attention_mask,
-            head_mask=head_mask,
-            inputs_embeds=inputs_embeds
-        )
-
-        hidden_states = distilbert_output[1]
-        h6 = hidden_states[-1][:, 0].reshape((-1, 1, 768))
-        h5 = hidden_states[-2][:, 0].reshape((-1, 1, 768))
-        h4 = hidden_states[-3][:, 0].reshape((-1, 1, 768))
-        h3 = hidden_states[-4][:, 0].reshape((-1, 1, 768))
-
-        hcat = torch.cat([h3, h4, h5, h6, category.reshape((-1, 1, 5))], 2)
-        
-        layer = self.pre_classifier(hcat)
-        layer = torch.nn.ReLU()(layer)
-        output = self.classifier(layer)
-
-        return output.reshape((-1, 30))
-
-    def init_weights(self):
-        self.distilbert.init_weights()
-        self.pre_classifier.reset_parameters()
-        self.classifier.reset_parameters()
+from src.model import model
 
 
 class Callback:
 
-    def spearman(self, redis_db, model, device, dataset, dataloader):
+    def spearman(self, model, device, dataset, dataloader):
         model.eval()
 
         with torch.no_grad():
@@ -69,21 +25,28 @@ class Callback:
             predicted = np.zeros((len(dataset), len(dataset.target)))
             target = np.zeros((len(dataset), len(dataset.target)))
             offset = 0
-            ids = dataset.ids
+
             for data in dataloader:
-                text = data[0].to(device)
-                mask = data[1].to(device)
-                category = data[2].to(device)
-                labels = data[-1].to(device)
-                output = model(text, attention_mask=mask, category=category)
+                features, labels = data
+                for field in features:
+                    features[field] = features[field].to(device)
+
+                for field in labels:
+                    labels[field] = labels[field].to(device)
+                label = labels['target_vector']
+
+                output = model(**features)
                 predicted[offset: offset + output.size()[0], :] = \
                     output.cpu().numpy()
-                target[offset: offset + labels.size()[0], :] = \
-                    labels.cpu().numpy()
+                target[offset: offset + label.size()[0], :] = \
+                    label.cpu().numpy()
                 offset += + output.size()[0]
-                print(offset)
+
+                print("Scored:", offset)
+
             for col in range(len(dataset.target)):
-                sprmn = scipy.stats.spearmanr(target[:,col], predicted[:,col])
+                sprmn = scipy.stats.spearmanr(target[:, col],
+                                              predicted[:, col])
                 spearman.append(sprmn.correlation)
             avg_spearman = np.average(np.array(spearman))
             print(avg_spearman, spearman)
@@ -112,8 +75,18 @@ class Callback:
                   f"{','.join((str(t) for t in self._core_temps()))}")
             time.sleep(wait_time)
 
+    def save(self, model, score, dir):
+        now = datetime.datetime.now()
+        timestamp = now.strftime('%d-%b-%Y_%H_%M')
+        rounded_score = str(round(score, 3)).replace('.', '-')
+        filename = f"{timestamp}_saved_model_{rounded_score}"
+        ouput_path = dir / filename
+
+        torch.save(model.state_dict(), ouput_path.resolve().as_posix())
+
 
 if __name__ == '__main__':
+    # Initialize and run training/test loops.
 
     # Load configuration file.
     paths = project_paths()
@@ -123,11 +96,12 @@ if __name__ == '__main__':
     with config_path.open(mode='r') as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
 
-    save_path = root_path / config['save']
+    save_dir = root_path / config['save']
 
     # Get training parameters.
     BATCH_SIZE = int(config['batch_size'])
-    GPU_CAP = int(config['gpu_capacity'])
+    GPU_CAP_TRAIN = int(config['gpu_capacity_train'])
+    GPU_CAP_TEST = int(config['gpu_capacity_test'])
     LEARNING_RATE = float(config['learning_rate'])
     EPOCHS = int(config['epochs'])
     FOLDS = int(config['folds'])
@@ -139,14 +113,15 @@ if __name__ == '__main__':
 
     # Connect to the Redis database that feeds the dataset object.
     r = redis_cnxn()
-    kfold_dataset = KFoldDataset(config_path, 5, r)
+
+    # Create train/test datasets for K-fold cross-validation.
+    kfold_dataset = KFoldDataset(config_path, FOLDS, r)
     train_folds = kfold_dataset.train_datasets
     test_folds = kfold_dataset.test_datasets
 
-    # Initialize model, loss function, and optimizer.
-
+    # Initialize model, loss function, and optimizer. Note Huggingface
+    # transformers are in eval mode by default.
     model_name = 'distilbert-base-uncased'
-
     model_config = transformers.DistilBertConfig.from_pretrained(
         model_name,
         output_hidden_states=True,
@@ -155,18 +130,17 @@ if __name__ == '__main__':
 
     print(model_config)
 
-    bnet = DistilBertForQUEST.from_pretrained(model_name, config=model_config)
+    bnet = model.DistilBertForQUEST.from_pretrained(model_name,
+                                                    config=model_config)
     bnet.to(device)
-    # Model is in eval mode by default
     bnet.train()
-    print("Model in training mode:", bnet.training)
 
-    # torch.cuda.empty_cache()
+    # Establish cost function and optimizer.
     criterion = torch.nn.BCEWithLogitsLoss()
     optimizer = torch.optim.SGD(bnet.parameters(), lr=0.001, momentum=0.9)
+
     callback = Callback()
     fold_spearman = []
-    # callback.spearman(r, bnet, device)
     for fold in range(FOLDS):
 
         # For each fold, reset model parameters and update the dataloaders
@@ -176,24 +150,26 @@ if __name__ == '__main__':
         train_dataset = train_folds[fold]
         test_dataset = test_folds[fold]
         trainloader = torch.utils.data.DataLoader(train_dataset,
-                                                  batch_size=GPU_CAP,
+                                                  batch_size=GPU_CAP_TRAIN,
                                                   shuffle=True,
-                                                  num_workers=0
+                                                  num_workers=2
                                                   )
         testloader = torch.utils.data.DataLoader(test_dataset,
-                                                 batch_size=30,
+                                                 batch_size=GPU_CAP_TEST,
                                                  shuffle=True,
-                                                 num_workers=0
+                                                 num_workers=2
                                                  )
+        # Calculate initial score on fold.
+        print(f"Calculating untrained score on fold {fold}.")
+        callback.spearman(bnet, device, test_dataset, testloader)
 
         for epoch in range(EPOCHS):
-            callback.spearman(r, bnet, device, test_dataset, testloader)
+
             # Check temperatures of GPU and CPU
             callback.cool(70, 70, 60)
 
             minibatches = 0
             running_loss = 0.0
-            sample_size = 0
 
             for i, data in enumerate(trainloader, 0):
 
@@ -201,20 +177,23 @@ if __name__ == '__main__':
                     callback.cool(75, 75, 60)
 
                 # Send inputs to GPU if GPU exists.
-                text = data[0].to(device)
-                mask = data[1].to(device)
-                category = data[2].to(device)
-                label = data[3].to(device)
+                features, labels = data
 
-                samples = data[0].shape[0]
+                for field in features:
+                    features[field] = features[field].to(device)
 
-                outputs = bnet(text, attention_mask=mask, category=category)
+                for field in labels:
+                    labels[field] = labels[field].to(device)
+                label = labels['target_vector']
+
+                samples = label.shape[0]
+
+                outputs = bnet(**features)
 
                 # If the batch size that the GPU can hold is smaller than the
                 # desired batch size, accumulate the gradients before updating
                 # weights.
-
-                update = BATCH_SIZE // GPU_CAP
+                update = BATCH_SIZE // GPU_CAP_TRAIN
                 remainder = (len(train_dataset) % BATCH_SIZE)
                 if i > len(train_dataset) - remainder:
                     scale = remainder / samples
@@ -227,17 +206,22 @@ if __name__ == '__main__':
 
                 if i % update == update - 1 or i == len(train_dataset) - 1:
                     optimizer.step()
-                    minibatches += 1
 
-                    # zero the parameter gradients
+                    # Zero the parameter gradients.
                     optimizer.zero_grad()
-                    print('[%d, %d, %5d] loss: %.3f' %
-                          (fold + 1, epoch + 1, i + 1,
+
+                    print('[Fold %d, Epoch %d, Minibatches %5d] BCE loss: '
+                          '%.3f' %
+                          (fold + 1, epoch + 1, minibatches + 1,
                            running_loss / (BATCH_SIZE)))
                     running_loss = 0.0
 
-        spearman = callback.spearman(r, bnet, device, test_dataset, testloader)
+                    minibatches += 1
+
+            spearman = callback.spearman(bnet, device, test_dataset,
+                                         testloader)
         fold_spearman.append(spearman)
+        callback.save(bnet, spearman, save_dir)
 
     print(fold_spearman)
     print('Finished Training')
